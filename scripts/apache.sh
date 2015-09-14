e646147bea82fe4ba48f6f2487a6e392ae575e92
Mike Bybee <me@mikebybee.com>
2015-09-14 11:35:04 -0500

    Nginx frontend to Apache

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -22,7 +22,7 @@ else
 fi
 
 if [[ -z $4 ]]; then
-    github_url="https://raw.githubusercontent.com/fideloper/Vaprobash/master"
+    github_url="https://raw.githubusercontent.com/stereoplegic/Vaprobash/master"
 else
     github_url="$4"
 fi
