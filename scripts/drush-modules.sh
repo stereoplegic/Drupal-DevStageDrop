fa521667f68d81ab29d933334380a6220592fedf
Mike Bybee <me@mikebybee.com>
2015-11-16 14:15:01 -0600

    Don't push live Vagrantfile

--- scripts/drush-modules.sh
+++ scripts/drush-modules.sh
@@ -1,4 +1,7 @@
 # Install drush modules - run unprivileged
+
+# Vagrant struggles to find composer path on provision, so use full path.
+# On subsequent "vagrant ssh" sessions, you can simply type "drush [optional command]".
 ~/.composer/vendor/bin/drush dl drush_extras --destination=$HOME/.drush
 ~/.composer/vendor/bin/drush dl coder --destination=$HOME/.drush
 ~/.composer/vendor/bin/drush dl registry_rebuild --destination=$HOME/.drush
