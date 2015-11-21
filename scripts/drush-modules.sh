779437817a394984d77e7f9adc96b420d32caa44
Mike Bybee <me@mikebybee.com>
2015-11-20 18:15:38 -0600

    drush improvements/fixes

--- scripts/drush-modules.sh
+++ scripts/drush-modules.sh
@@ -1,12 +1,17 @@
+#!/usr/bin/env bash
+
 # Install drush modules - run unprivileged
 
 # Vagrant struggles to find composer path on provision, so use full path.
 # On subsequent "vagrant ssh" sessions, you can simply type "drush [optional command]".
-~/.composer/vendor/bin/drush dl drush_extras --destination=$HOME/.drush
-~/.composer/vendor/bin/drush dl coder --destination=$HOME/.drush
-~/.composer/vendor/bin/drush dl registry_rebuild --destination=$HOME/.drush
-~/.composer/vendor/bin/drush dl hacked --destination=$HOME/.drush
-~/.composer/vendor/bin/drush dl site_audit --destination=$HOME/.drush
-~/.composer/vendor/bin/drush dl module_builder --destination=$HOME/.drush
+
+# No 8.x version of these modules exist yet, so append "-7.x" to the module name for the time being.
+~/.composer/vendor/bin/drush dl -y drush_extras-7.x --destination=$HOME/.drush
+~/.composer/vendor/bin/drush dl -y coder-7.x --destination=$HOME/.drush
+~/.composer/vendor/bin/drush dl -y registry_rebuild-7.x --destination=$HOME/.drush
+~/.composer/vendor/bin/drush dl -y hacked-7.x --destination=$HOME/.drush
+~/.composer/vendor/bin/drush dl -y site_audit-7.x --destination=$HOME/.drush
+~/.composer/vendor/bin/drush dl -y module_builder-7.x --destination=$HOME/.drush
 
 ~/.composer/vendor/bin/drush cc drush
+
